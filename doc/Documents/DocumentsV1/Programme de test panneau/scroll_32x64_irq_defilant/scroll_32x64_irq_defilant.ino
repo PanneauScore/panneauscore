0b08deb44a7e9a6e691404f25a9cadb102ed0e5a
Panneau Score <58820387+PanneauScore@users.noreply.github.com>
2019-12-16 08:54:55 +0100

    Add--/Doc

---
+++ doc/Documents/DocumentsV1/Programme de test panneau/scroll_32x64_irq_defilant/scroll_32x64_irq_defilant.ino
@@ -0,0 +1,91 @@
+// testshapes demo for RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 32x64 RGB LED matrix.
+
+// NOTE THIS CAN ONLY BE USED ON A MEGA! NOT ENOUGH RAM ON UNO!
+
+
+#include <TimerThree.h>
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
+
+#define OE   9
+#define LAT 10
+#define CLK 11
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+#define sync   2
+
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, true, 64);   //true : preparation avant affichage
+
+char car = ' ';
+char str[100] = " ";
+char tab_car[100] = " ";
+int indice = 0;
+int textX = matrix.width();
+int textMin;
+byte flag = 0;
+byte etat_premier_caractere = 0;
+
+void setup() {
+  Serial.begin(57600);
+  matrix.begin();
+  matrix.setTextWrap(false); // Allow text to run off right edge
+  matrix.setTextSize(1);
+  pinMode(sync, OUTPUT);
+  Timer3.initialize(55000); // set a timer of length 40ms
+  Timer3.attachInterrupt(timerIsr);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    reception_caractere();
+  }
+
+  if (flag == 1) {
+    matrix.fillScreen(0);
+    matrix.setTextColor(matrix.Color333(7, 7, 7));
+    matrix.setCursor(textX, 3);
+    matrix.print(str);
+    matrix.swapBuffers(true);
+    flag = 0;
+  }
+}
+
+void reception_caractere()
+{
+  car = Serial.read();
+  if ((car == '$') || (etat_premier_caractere == 1))
+  { etat_premier_caractere = 1;
+
+    if ((car != 10) && (car != '$'))
+    { tab_car[indice] = car;
+      Serial.print(tab_car[indice]); Serial.print("    ");
+      Serial.print("indice:"); Serial.println(indice);
+      indice++;
+    }
+    else if (car == 10)
+    { tab_car[indice] = 0;
+      for (int n = 0; n <= indice; n++)
+      { str[n] = tab_car[n];
+      }
+      textMin = indice * -6;
+      indice = 0;
+      etat_premier_caractere = 0;
+    }
+  }
+}
+
+void timerIsr()
+{
+  // Toggle sync
+  digitalWrite(sync, digitalRead(sync) ^ 1);
+  if ((--textX) < textMin) textX = matrix.width();
+  flag = 1;
+}
+
