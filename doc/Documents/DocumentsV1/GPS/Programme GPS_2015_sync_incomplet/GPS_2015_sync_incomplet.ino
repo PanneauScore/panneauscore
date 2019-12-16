2c4a4b78b6b6d626f25f2f4bfde2bad3d1e07b93
Panneau Score <58820387+PanneauScore@users.noreply.github.com>
2019-12-16 09:00:41 +0100

    Add--/Doc

---
+++ doc/Documents/DocumentsV1/GPS/Programme GPS_2015_sync_incomplet/GPS_2015_sync_incomplet.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd (12, 11, 6, 5, 4, 3);  // initialize the library with the numbers of the interface pins
+
+/************Sous-programme d'initialisation*******************/
+void setup()
+{ 
+   Serial.begin(4800);   // initialize serial communication at 4800 for the GPS
+   lcd.begin(16, 2);     // Initialise le nombre de lignes et de colonnes 
+   lcd.clear();          // Efface l'afficheur et positionne le curseur en (0,0)
+} 
+
+/*************/
+void loop() 
+{ 
+  String trameGPS_recue;
+  const int lf=10;
+  int virgule[12];
+  char sync;
+  
+    if (Serial.available()>0)          // Vérifie qu'une communication série est présente
+      {
+        sync=Serial.read();            // Place chaque caractère reçu un à un dans la variable sync
+        if (sync=='$')                 // Vérifie que la variable sync correspond au caractère $: synchronisation au début d'une trame
+        {
+           trameGPS_recue = Serial.readStringUntil(lf);      // lit la trame complète et la stocke dans la chaîne trameGPS_recue
+       //  trameGPS_recue = "$GPGGA,152241.000,4759.7067,N,00012.2782,E,0,4,5.2,100.0,M,12.3,M,,*6A" ;  // Test avec une trame contenant FIX=0               
+           if (trameGPS_recue.substring(0,5) == "GPGGA")     // Vérifie si la trame complète commence par GPGGA : Synchronisation de la trame $GPGGA
+             { 
+     
+
+
+
+
+
+
+
+
+             }
+        }
+      }
+   }
+
+
+ 
+
+
+
+
---
+++ doc/Documents/DocumentsV1/GPS/gps/gps.ino
@@ -0,0 +1,121 @@
+
+#include <SoftwareSerial.h>
+
+#define txPin 9      //tx pin into RX GPS connection
+#define rxPin 8      //rx pin into TX GPS connection
+
+SoftwareSerial serialGps(rxPin, txPin);  // RX, TX for GPS
+
+String time;
+String flag;
+String date;
+String lat;
+String ns;
+String lon;
+String ew;
+String spd;
+String alt;
+String course;
+String sats;
+
+
+String readParamGps() {
+  return  serialGps.readStringUntil(',');
+}
+
+
+bool getGps () {
+  while (serialGps.read() != '$') {
+    delay(1);
+    //Serial.println('S');
+  } //Wait for start
+  String nmea = readParamGps();
+  if (nmea == "GPRMC") {
+    //Process a GPS recommended minimum data message.
+    readParamGps(); //skip any parameters not required
+    flag = readParamGps();
+    lat = readParamGps();
+    ns = readParamGps();
+    lon = readParamGps();
+    ew = readParamGps();
+    spd = readParamGps();
+    course = readParamGps(); //skip any parameters not required
+    date = readParamGps();
+  }
+  if (nmea == "GPGGA") {
+    //Process a GPS fix data message.
+    time = readParamGps();
+    readParamGps(); //skip any parameters not required
+    readParamGps();
+    readParamGps();
+    readParamGps();
+    readParamGps();
+    //    fix = readParam();
+    sats = readParamGps();
+    readParamGps();
+    alt = readParamGps();
+    if (flag == "A") { //If the data is valid it is good to go
+      return true;
+    }
+  }
+  return false;
+}
+
+
+void printGps()
+{
+  Serial.print(date);
+  Serial.print(",");
+  Serial.print(time);
+  Serial.print(",");
+  Serial.print(flag);
+  Serial.print(",");
+  Serial.print(lat);
+  Serial.print(ns);
+  Serial.print(",");
+  Serial.print(lon);
+  Serial.print(ew);
+  Serial.print(",");
+  Serial.print(spd);
+  Serial.print("knots,");
+  Serial.print(course);
+  Serial.print(" degrees,");
+  Serial.print(alt);
+  Serial.print("m,");
+  Serial.println(sats);
+}
+
+
+void gpsDecimal(String longitud, String ew, String latitud, String ns)
+{
+  double longitude, latitude;
+
+  longitude = longitud.substring(0, 3).toFloat() + (longitud.substring(3, 10).toFloat() / 60);
+  latitude = latitud.substring(0, 2).toFloat() + (latitud.substring(2, 9).toFloat() / 60);
+
+  if (ew.equals("W")) longitude = longitude * -1;
+  if (ns.equals("S"))  latitude = latitude * -1;
+
+
+  Serial.println(longitude, 5);
+  Serial.println(latitude, 5);
+
+}
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  serialGps.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (getGps()) {
+    printGps();
+    gpsDecimal(lon, ew, lat, ns);
+  }
+}
